6ff78a11c95ba9fcc15de04542c238b0411fc4e6
gggui <g_guichaoua@yahoo.fr>
2015-12-01 10:14:07 +0100

    initialisation prog .py

---
+++ programme_turtle.py
@@ -0,0 +1,8 @@
+""" Mini projet Turtle par ..."""
+
+#ecrire une procédure carré(c) qui trace un carré de coté c
+
+#ecrire une procédure triangle_equi(c) qui trace un triangle équilatéral de coté c
+
+#ecrire un script permettant de tracer la première frise demandée
+
